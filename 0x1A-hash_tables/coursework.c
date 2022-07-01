36bd4cffd3e662894c95758e835c5076b6f20d20
root <root@DESKTOP-TNPT3UH.localdomain>
2022-07-01 11:25:17 +0300

    added the logical bound comment

--- 0x1A-hash_tables/coursework.c
+++ 0x1A-hash_tables/coursework.c
@@ -16,7 +16,7 @@ int main(){
 
 double v,h,radius;
 v = (0.001 * 0.5 * 280);
-h = 1.0;
+h = 1.0; // You can  add the logical bound  here to protect the program  form negative and zero  heights
 radius = find_radius(v,h);
 
 printf("The radius of the  saucepan to be used is  %.2f",radius);
