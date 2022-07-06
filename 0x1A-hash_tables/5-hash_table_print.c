edf1f186b5354e9dbe4e9829bd59ef6dc13b2f02
Michael Goboola <47690301+michaelgobz@users.noreply.github.com>
2022-07-06 13:11:01 +0000

    rethinking over the tasks fixed the logical bugs

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,30 +1,39 @@
-#include "hash_tables.h"
+#include "./hash_tables.h"
 
 /**
- * hash_table_print - prints a hash table
- * @ht: pointer to hash table
- * Return: void
+ * hash_table_print - Prints a hash table.
+ * @ht: A pointer to the hash table to print.
+ *
+ * Description: Key/value pairs are printed in the order
+ *              they appear in the array of the hash table.
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned long int i = 0;
-	hash_node_t *node;
-	char *comma = "";
+    hash_node_t *node;
+    unsigned long int i;
+    unsigned char comma_flag = 0;
 
-	if (!ht || !ht->array)
-		return;
+    if (ht == NULL)
+        return;
 
-	putchar('{');
-	while (i < ht->size)
-	{
-		node = (ht->array)[i];
-		while (node)
-		{
-			printf("%s'%s': '%s'", comma, node->key, node->value);
-			comma = ", ";
-			node = node->next;
-		}
-		i++;
-	}
-	puts("}");
+    printf("{");
+    for (i = 0; i < ht->size; i++)
+    {
+        if (ht->array[i] != NULL)
+        {
+            if (comma_flag == 1)
+                printf(", ");
+
+            node = ht->array[i];
+            while (node != NULL)
+            {
+                printf("'%s': '%s'", node->key, node->value);
+                node = node->next;
+                if (node != NULL)
+                    printf(", ");
+            }
+            comma_flag = 1;
+        }
+    }
+    printf("}\n");
 }
--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,35 +1,30 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete - deletes a hash table
- * @ht: pointer to hash table
- * Return: void
+ * hash_table_delete - Deletes a hash table.
+ * @ht: A pointer to a hash table.
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	unsigned long int i = 0;
-	hash_node_t *node, *next;
+    hash_table_t *head = ht;
+    hash_node_t *node, *tmp;
+    unsigned long int i;
 
-	if (ht == NULL || ht->array == NULL)
-		return;
-
-	while (i < ht->size)
-	{
-		node = (ht->array)[i];
-		while (node)
-		{
-			next = node->next;
-			if (node->key)
-				free(node->key);
-			if (node->value)
-				free(node->value);
-			node->key = NULL;
-			node->value = NULL;
-			free(node);
-			node = next;
-		}
-		i++;
-	}
-	free(ht->array);
-	free(ht);
+    for (i = 0; i < ht->size; i++)
+    {
+        if (ht->array[i] != NULL)
+        {
+            node = ht->array[i];
+            while (node != NULL)
+            {
+                tmp = node->next;
+                free(node->key);
+                free(node->value);
+                free(node);
+                node = tmp;
+            }
+        }
+    }
+    free(head->array);
+    free(head);
 }
